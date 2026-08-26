2666c55987ddab1eb2211c7ef673c02f0a4e445f
nadhiraatanacio-oss <nadhira.atanacio@tecsup.edu.pe>
2026-08-26 10:09:06 -0500

    feat: agregar ejercicio de calculo con descuento e igv

---
+++ Semana01/Calcular.playground/Contents.swift
@@ -0,0 +1,38 @@
+import Foundation
+
+//un calendario de pagos
+//ingresar el nombre del producto, precio y cantidad
+let igv = 0.18
+func CalcularDescuesto (cantidad: Int, subtotal: Double)-> Double {
+    if cantidad > 10 {
+        return subtotal * 0.15
+    }else if cantidad > 5 {
+        return subtotal * 0.10
+    }else
+    {
+        return 0.0
+    }
+}
+print ("ingrese el nombre del producto")
+let producto = readLine() ?? ""
+print ("Ingrese el precio de producto")
+let precio = Double(readLine() ?? "0") ?? 0.0
+print ("ingrese la cantidad de producto")
+let cantidad = Int(readLine() ?? "0") ?? 0
+
+let subtotal = precio * Double(cantidad)
+let descuento = CalcularDescuesto(cantidad: cantidad, subtotal: subtotal)
+let montobase = subtotal - descuento
+let igvcalculo = montobase * igv
+let total = montobase + igvcalculo
+
+//n
+
+
+// hallar el monto base: subtotal - descuento
+//hallar el igv : montobase * igv
+//toal monto base + igv
+//commit
+//mostrar los datos
+
+
