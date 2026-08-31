bd1a518addf8bf3ea5afe6fb186bf54454a34d46
Alejandra Atanacio <nadhira.atanacio@tecsup.edu.pe>
2026-08-31 15:49:08 -0500

    "feat: Crear proyecto Lab02 y Playground en Semana02"

---
+++ Semana02/Lab02-Condicionales/Lab02-Condicionales/Lab02_CondicionalesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Lab02_CondicionalesApp.swift
+//  Lab02-Condicionales
+//
+//  Created by Tecsup on 31/08/26.
+//
+
+import SwiftUI
+
+@main
+struct Lab02_CondicionalesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
