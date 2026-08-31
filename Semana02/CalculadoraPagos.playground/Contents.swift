d13bc7dc8afc54807a3a79a6a826dea8db184fad
Alejandra Atanacio <nadhira.atanacio@tecsup.edu.pe>
2026-08-31 15:02:13 -0500

    commit 02_Pagos

--- Semana02/CalculadoraPagos.playground/Contents.swift
+++ Semana02/CalculadoraPagos.playground/Contents.swift
@@ -1,3 +1,4 @@
+
 import Foundation
 
 // Datos del producto
@@ -25,4 +26,12 @@ while plan != 6 && plan != 12 && plan != 24 {
 
 print("Plan seleccionado: \(plan) meses")
 
+// Pago adelantado
+print("¿En qué mes hará un pago adelantado? (0 ninguno): ", terminator: "")
+let mesAdelanto = Int(readLine() ?? "") ?? 0
 
+var montoAdicional = 0.0
+if mesAdelanto > 0 {
+    print("Monto Adicional a pagar ese mes (S/.): ", terminator: "")
+    montoAdicional = Double(readLine() ?? "") ?? 0.0
+}
