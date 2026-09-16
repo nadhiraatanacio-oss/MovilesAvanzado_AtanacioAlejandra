a5c4f9fd8b2425eabe41f791918261be120cd9ad
Alejandra Atanacio <nadhira.atanacio@tecsup.edu.pe>
2026-09-15 20:12:27 -0500

    feat: Caso 2A - biblioteca sin IA

--- Semana04/Laboratorio4/Lab04.playground/Contents.swift
+++ Semana04/Laboratorio4/Lab04.playground/Contents.swift
@@ -182,3 +182,14 @@ class Biblioteca {
         }
     }
 }
+let biblioteca = Biblioteca()
+biblioteca.agregar(libro: Libro(titulo: "Cien años de soledad", autor: "Gabriel García Márquez"))
+biblioteca.agregar(libro: Libro(titulo: "La ciudad y los perros", autor: "Mario Vargas Llosa"))
+biblioteca.agregar(libro: Libro(titulo: "El Quijote", autor: "Miguel de Cervantes"))
+
+biblioteca.prestar(titulo: "La ciudad y los perros")
+biblioteca.prestar(titulo: "La ciudad y los perros")
+biblioteca.devolver(titulo: "La ciudad y los perros")
+biblioteca.prestar(titulo: "El Quijote")
+biblioteca.prestar(titulo: "El Principito")
+biblioteca.inventario()
