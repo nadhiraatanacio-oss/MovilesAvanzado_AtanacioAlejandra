37d119a58ece97676d58f1117eb707c69441b214
Alejandra Atanacio <nadhira.atanacio@tecsup.edu.pe>
2026-09-15 23:48:35 -0500

    Commit01:estructura de datos de cursos

--- Semana04/Actividad01/actividades/Caso01.playground/Contents.swift
+++ Semana04/Actividad01/actividades/Caso01.playground/Contents.swift
@@ -1,3 +1,17 @@
-import UIKit
+// ===== ACTIVIDAD PROPUESTA 01: FACTURA DE CURSOS =====
 
-var greeting = "Hello, playground"
+struct Curso {
+    let nombre: String
+    let precioUnitario: Double
+    let cantidad: Int
+}
+
+let nombreEstudiante = "Juan León"
+let dni = "78965412"
+let esAlumnoTecsup = true
+
+let cursos: [Curso] = [
+    Curso(nombre: "Swift Avanzado", precioUnitario: 450.0, cantidad: 1),
+    Curso(nombre: "IA con Python", precioUnitario: 650.0, cantidad: 2),
+    Curso(nombre: "Diseño UX/UI", precioUnitario: 500.0, cantidad: 1)
+]
